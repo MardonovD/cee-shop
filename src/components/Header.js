6b46ceed352cafd2394e20de66bf493f1dfa750a
Dostonbek <mardonovdoston69@gmail.com>
2022-05-15 08:45:34 +0300

    btn

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,11 @@ import React from "react";
 const Header = () => {
   return (
     <div className="ui pointing menu">
-      <a className="active item">Doston Shoop</a>
+      <a className="active item">Doston Shoop </a>
 
-      <div className="right menu"></div>
+      <div className="right menu">
+        <button className="btn btn-danger">Save</button>
+      </div>
     </div>
   );
 };
